c741e4b3b4403f96fb7e8f6c7c026c31d963aa13
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-08-27 06:24:58 +0600

    Load more data, more APIs, send data to function

--- js/fetch.js
+++ js/fetch.js
@@ -5,8 +5,8 @@ fetch('https://jsonplaceholder.typicode.com/todos/1')
 
 fetch('') 
 */
-const url = 'https://jsonplaceholdr.typicode.com/todos/1';
 
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
 // fetch(url)
 //     .then(response => response.json())
 //     .then(json => console.log(json))
